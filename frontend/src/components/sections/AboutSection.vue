f3261b830a262af40040ed4be6f7b3e7abba2a76
M.PIETTE <62299589+Pousset@users.noreply.github.com>
2024-11-14 16:45:18 +0100

    Format

--- frontend/src/components/sections/AboutSection.vue
+++ frontend/src/components/sections/AboutSection.vue
@@ -1,9 +1,19 @@
 <template>
-    <div class="max-w-2xl text-center px-4 ">
-        <h2 class="text-2xl md:text-3xl font-bold text-gray-900 dark:text-white mb-4 md:mb-6">
-            About Me
-        </h2>
-        <p class="text-sm md:text-lg text-gray-600 dark:text-gray-300">Bonjour! Je m’appelle Mathis PIETTE, je suis un développeur web full stack passionné qui s'intéresse de près au design d'interface. Ma spécialité est de construire des projets web de A à Z, en commençant par la définition du produit pour répondre au mieux aux besoins, en passant par la conception de l'outil, l'intégration des pages et le développement des différentes fonctionnalités. Sans oublier l'optimisation du référencement naturel et la mise en ligne. Je peux donc vous accompagner sur toutes les étapes de votre projet digital. Voici mes domaines de compétences... 
-        </p>
-    </div>
+  <div class="max-w-2xl text-center px-4">
+    <h2
+      class="text-2xl md:text-3xl font-bold text-gray-900 dark:text-white mb-4 md:mb-6"
+    >
+      About Me
+    </h2>
+    <p class="text-sm md:text-lg text-gray-600 dark:text-gray-300">
+      Bonjour! Je m’appelle Mathis PIETTE, je suis un développeur web full stack
+      passionné qui s'intéresse de près au design d'interface. Ma spécialité est
+      de construire des projets web de A à Z, en commençant par la définition du
+      produit pour répondre au mieux aux besoins, en passant par la conception
+      de l'outil, l'intégration des pages et le développement des différentes
+      fonctionnalités. Sans oublier l'optimisation du référencement naturel et
+      la mise en ligne. Je peux donc vous accompagner sur toutes les étapes de
+      votre projet digital. Voici mes domaines de compétences...
+    </p>
+  </div>
 </template>
