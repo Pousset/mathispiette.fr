0306ae7cf7a2fe106bc4cfcec510e8e7918a7c3c
M.PIETTE <62299589+Pousset@users.noreply.github.com>
2024-12-04 08:53:03 +0100

    supp colorManager.js + refacto

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,13 +2,12 @@
 import { onMounted } from 'vue'
 import { loadSavedColor } from './utils/ButtonColorManager'
 import { initializeTheme } from './utils/themeManager'
-import { loadSavedDevWebColor } from './utils/DevWebColorManager'
 import ThemeCustomizer from './components/ui/ThemeCustomizer.vue'
 
 onMounted(() => {
   loadSavedColor()
   initializeTheme()
-  loadSavedDevWebColor()
+
 })
 </script>
 <template>
