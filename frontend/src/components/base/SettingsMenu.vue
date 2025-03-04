7a580906090e4bb6657f91a58778ece7e654a562
M.PIETTE <62299589+Pousset@users.noreply.github.com>
2025-03-04 12:37:32 +0100

    Close le menu

--- frontend/src/components/base/SettingsMenu.vue
+++ frontend/src/components/base/SettingsMenu.vue
@@ -155,6 +155,27 @@ onUnmounted(() => {
       @mousedown="startDrag"
     >
       <div class="p-4 space-y-6" role="none">
+        <!-- Bouton pour fermer le menu -->
+        <button
+          class="absolute top-2 right-2 p-2 rounded-full bg-gray-200 hover:bg-gray-300 dark:bg-gray-700 dark:hover:bg-gray-600 transition-all duration-300"
+          @click="toggle"
+        >
+          <span class="sr-only">Fermer</span>
+          <svg
+            class="w-4 h-4"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M6 18L18 6M6 6l12 12"
+            ></path>
+          </svg>
+        </button>
         <div @click="handleScrollBarToggleClick">
           <ScrollBarToggle />
         </div>
